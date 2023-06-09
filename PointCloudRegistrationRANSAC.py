e77bf105bf8df534f1be37294363347e309db06a
Dmitry Isaev <dmitry.isaev@duke.edu>
2023-06-09 17:12:38 -0400

    in progress - point cloud RANSAC

---
+++ PointCloudRegistrationRANSAC.py
@@ -0,0 +1,74 @@
+# RANSAC Global Registration of two point clouds. Totally copied from tutorial on Open3D website.
+# http://www.open3d.org/docs/release/tutorial/pipelines/global_registration.html
+
+import open3d as o3d
+import numpy as np
+import copy
+
+def draw_registration_result(source, target, transformation):
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0.706, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    source_temp.transform(transformation)
+    o3d.visualization.draw_geometries([source_temp, target_temp],
+                                      zoom=0.4559,
+                                      front=[0.6452, -0.3036, -0.7011],
+                                      lookat=[1.9892, 2.0208, 1.8945],
+                                      up=[-0.2779, -0.9482, 0.1556])
+    
+
+def preprocess_point_cloud(pcd, voxel_size):
+    print(":: Downsample with a voxel size %.3f." % voxel_size)
+    pcd_down = pcd.voxel_down_sample(voxel_size)
+
+    radius_normal = voxel_size * 2
+    print(":: Estimate normal with search radius %.3f." % radius_normal)
+    pcd_down.estimate_normals(
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+
+    radius_feature = voxel_size * 5
+    print(":: Compute FPFH feature with search radius %.3f." % radius_feature)
+    pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
+        pcd_down,
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+    return pcd_down, pcd_fpfh
+
+def execute_global_registration(source_down, target_down, source_fpfh,
+                                target_fpfh, voxel_size):
+    distance_threshold = voxel_size * 1.5
+    print(":: RANSAC registration on downsampled point clouds.")
+    print("   Since the downsampling voxel size is %.3f," % voxel_size)
+    print("   we use a liberal distance threshold %.3f." % distance_threshold)
+    result = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
+        source_down, target_down, source_fpfh, target_fpfh, True,
+        distance_threshold,
+        o3d.pipelines.registration.TransformationEstimationPointToPoint(False),
+        3, [
+            o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(
+                0.9),
+            o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(
+                distance_threshold)
+        ], o3d.pipelines.registration.RANSACConvergenceCriteria(100000, 0.999))
+    return result
+
+if __name__ == '__main__':
+    # Load the two point clouds
+    source = o3d.io.read_point_cloud("/diskB/data/tmp/left_forReg.ply")
+    target = o3d.io.read_point_cloud("/diskB/data/tmp/right_forReg.ply")
+    #source_cuda = source.cuda(0)
+    #target_cuda = target.cuda(0)
+    voxel_size = 5.0  
+    source_down, source_fpfh = preprocess_point_cloud(source, voxel_size)
+    target_down, target_fpfh = preprocess_point_cloud(target, voxel_size)
+    result_ransac = execute_global_registration(source_down, target_down,
+                                                source_fpfh, target_fpfh,
+                                                voxel_size)
+    print(result_ransac)
+    draw_registration_result(source_down, target_down, result_ransac.transformation)
+
+#    pcd2_transformed = register_point_clouds (pcd1,pcd2)
+#    o3d.io.write_point_cloud("/diskB/data/tmp/right_forReg_transformed.ply", pcd2_transformed)
+
+    # Visualize the registered point clouds
+    #o3d.visualization.draw_geometries([pcd1, pcd2])
